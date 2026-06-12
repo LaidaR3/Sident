cadcf6cdeaa4aa12a038e6195a275ec6bc652158
Laida Rusinovci <149337343+LaidaR3@users.noreply.github.com>
2026-06-12 12:29:44 +0200

    fix services build

--- dental-clinic/app/services/page.tsx
+++ dental-clinic/app/services/page.tsx
@@ -1,14 +1,20 @@
+import { Suspense } from "react";
 import ServicesHero from "@/components/services/ServiceHero";
 import ServicesGrid from "@/components/services/Services";
 import TreatmentProcess from "@/components/services/Process";
 import FAQSection from "@/components/services/FAQ";
-export default function Services(){
-    return (
-        <main>
-            <ServicesHero />
-            <ServicesGrid />
-            <TreatmentProcess />
-            <FAQSection />
-        </main>
-    )
+
+export default function Services() {
+  return (
+    <main>
+      <ServicesHero />
+
+      <Suspense fallback={null}>
+        <ServicesGrid />
+      </Suspense>
+
+      <TreatmentProcess />
+      <FAQSection />
+    </main>
+  );
 }
